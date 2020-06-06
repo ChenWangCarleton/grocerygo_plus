df3ad42ecc6d4a735b35902b65a3db41ce683365
ChenWangCarleton <wch4university@gmail.com>
2020-06-06 13:06:59 -0400

    solution for getting item price for all store locations of loblaws

--- grocerygo/web_crawler/test.py
+++ grocerygo/web_crawler/test.py
@@ -26,7 +26,7 @@ link.click()
 element_present = EC.presence_of_element_located((By.CLASS_NAME, 'location-list-item__actions'))
 WebDriverWait(driver, 10).until(element_present)
 a = driver.find_elements_by_class_name("location-list-item__actions")
-a[0].click()
+a[3].click()
 element_present = EC.presence_of_element_located((By.LINK_TEXT, 'Continue'))
 WebDriverWait(driver, 10).until(element_present)
 link = driver.find_element_by_link_text('Continue')
@@ -34,8 +34,45 @@ link.click()
 
 print(driver.title)
 cookies_list = driver.get_cookies()
+isabella_dict = {}
+for cookie in cookies_list:
+    isabella_dict[cookie['name']] = cookie['value']
+
+print(isabella_dict)
+#driver.get("https://www.loblaws.ca/Food/Bakery/Desserts-%26-Pastries/Pastries-%26-Danishes/Sikorski-Sausages-Double-Smoked-Bacon/p/20996153_KG")
+
+
+driver_2 = webdriver.Chrome(web_driver_loc)
+driver_2.get("https://www.loblaws.ca/Food/Deli/Deli-Meats/Beef/Ziggy's-Smoked-Beef-Pastrami/p/21020569_KG")
+print(driver_2.title)
+cookies_list = driver_2.get_cookies()
 cookies_dict = {}
 for cookie in cookies_list:
     cookies_dict[cookie['name']] = cookie['value']
 
 print(cookies_dict)
+element_present = EC.presence_of_element_located((By.LINK_TEXT, 'choose the location'))
+WebDriverWait(driver_2, 10).until(element_present)
+link = driver_2.find_element_by_link_text('choose the location')
+link.click()
+element_present = EC.presence_of_element_located((By.CLASS_NAME, 'location-list-item__actions'))
+WebDriverWait(driver_2, 10).until(element_present)
+a = driver_2.find_elements_by_class_name("location-list-item__actions")
+a[0].click()
+element_present = EC.presence_of_element_located((By.LINK_TEXT, 'Continue'))
+WebDriverWait(driver_2, 10).until(element_present)
+link = driver_2.find_element_by_link_text('Continue')
+link.click()
+
+print(driver_2.title)
+cookies_list = driver_2.get_cookies()
+bank_dict = {}
+for cookie in cookies_list:
+    bank_dict[cookie['name']] = cookie['value']
+
+print(bank_dict)
+
+set1 = set(bank_dict.items())
+set2 = set(isabella_dict.items())
+print(set1 ^ set2)
+driver.get("https://www.loblaws.ca/Food/Deli/Deli-Meats/Beef/Ziggy's-Smoked-Beef-Pastrami/p/21020569_KG")
