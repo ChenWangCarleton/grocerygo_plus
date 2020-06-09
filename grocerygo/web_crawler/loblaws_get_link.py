3e5f24e9c9c9cab873eecdb73a6ef3ed11b9af64
ChenWangCarleton <wch4university@gmail.com>
2020-06-09 12:17:29 -0400

    finished load all item in a page function

--- grocerygo/web_crawler/loblaws_get_link.py
+++ grocerygo/web_crawler/loblaws_get_link.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 import logging
 import traceback
+import time
 
 import os
 
@@ -25,7 +26,49 @@ web_driver_loc = os.path.join(os.path.abspath(os.path.dirname(__file__)),'chrome
 max_running_thread = 10
 waiting_list = [] # a list contains thread to be executed to get item links of each leaf categories
 
-def has_more_subcategories(url_category_tuple,headless=False): #
+def load_more(driver):
+    """
+    This function loads all the "load xxx more results" on the page untill all the items are loaded
+    Then it returns the total times of load button clicked successfully if there was any
+    Otherwise it returns 0
+
+    The function is implemented by checking if backslash exists in pagination,
+    no backslash example(1-166 Results) with backslash example (1-48 / 156 Results)
+    if there is backslash in pagination, then it means load more button should be clickable,
+    click and repeat untill no more backslash in pagination or time limit reached
+    :param driver: the driver instance that should be the leaf category webpage
+    :return:
+        int
+            number of times the load more button been pressed
+    """
+    try:
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'pagination'))
+        WebDriverWait(driver, 10).until(element_present)
+    except:
+        logger.error('time limit exceeds when waiting for loading total number of items\n{}\n{}'.driver.getCurrentUrl(), traceback.format_exc())
+    current_pagination = driver.find_element_by_class_name('pagination').text
+    click_counter = 0
+
+    while '/' in current_pagination:
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'load-more-button'))
+            WebDriverWait(driver, 10).until(element_present)
+        except:
+            logger.error('time limit exceeds when waiting for load more buttion to appear on page\n{}\n{}'.format(driver.getCurrentUrl(), traceback.format_exc()))
+            return False
+        load_more_button = driver.find_element_by_class_name('load-more-button')
+        load_more_button.click()
+        time.sleep(1)
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'pagination'))
+            WebDriverWait(driver, 10).until(element_present)
+        except:
+            logger.error('time limit exceeds when waiting for loading pagination after clicking load more button. button click counter {}\n{}\n{}'.format(click_counter,driver.getCurrentUrl(),
+                traceback.format_exc()))
+        current_pagination = driver.find_element_by_class_name('pagination').text
+        click_counter = click_counter + 1
+    return click_counter
+def has_more_subcategories(url_category_tuple,headless=False,disableimage=False): #
     """
     This function checks if there are more subcategories in the url.
     If there is any, it returns the list of 2 element tuple. The first element in the tuple is the url,
@@ -39,15 +82,15 @@ def has_more_subcategories(url_category_tuple,headless=False): #
             string of the current category if there is no more subcategories
     """
     url = url_category_tuple[0]
-
+    options = Options()
     if headless:
-        options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')  # Last I checked this was necessary.
         options.add_argument("window-size=1920,1080")
-        driver = webdriver.Chrome(web_driver_loc, chrome_options=options)
-    else:
-        driver = webdriver.Chrome(web_driver_loc)
+    if disableimage:
+        options.add_argument('--blink-settings=imagesEnabled=false')
+
+    driver = webdriver.Chrome(web_driver_loc, options=options)
     driver.get(url)
     try:
         element_present = EC.presence_of_element_located((By.CLASS_NAME, 'category-filter__subcategories'))
@@ -79,4 +122,12 @@ if __name__ == '__main__':
     freshfromscratch = 'https://www.loblaws.ca/Food/Meal-Kits/By-Serving-Time/Fresh-from-Scratch-(15-Min%2B)/plp/LSL001002009003?navid=CLP-L5-Fresh-from-Scratch-15-Min'
     fruit = 'https://www.loblaws.ca/Food/Fruits-%26-Vegetables/Fruit/c/LSL001001001000?navid=CLP-L4-Fruit'
     apple = 'https://www.loblaws.ca/Food/Fruits-%26-Vegetables/Fruit/Apples/plp/LSL001001001001?navid=CLP-L5-Apples'
-    print(has_more_subcategories((apple,[])))
+    #print(has_more_subcategories((apple,[])))
+
+    options = Options()
+    options.add_argument('--blink-settings=imagesEnabled=false')
+    driver = webdriver.Chrome(web_driver_loc, options=options)
+    driver.get(fruit)
+    print(load_more(driver))
+    driver.get(apple)
+    print(load_more(driver))
--- tests/test_loblaws_get_link.py
+++ tests/test_loblaws_get_link.py
@@ -1,6 +1,31 @@
-from grocerygo_plus.grocerygo.web_crawler import has_more_subcategories
+from grocerygo_plus.grocerygo.web_crawler import has_more_subcategories, load_more
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+import os
 def test_has_more_subcategories():
     food = 'https://www.loblaws.ca/Food/c/LSL001000000000?navid=flyout-L2-Food'
     apple = 'https://www.loblaws.ca/Food/Fruits-%26-Vegetables/Fruit/Apples/plp/LSL001001001001?navid=CLP-L5-Apples'
-    assert has_more_subcategories((apple,[]),headless=True) == 'Apples'
-    assert len(has_more_subcategories((food,[]),headless=True)) == 15
+    assert has_more_subcategories((apple, [])) == 'Apples'
+    assert has_more_subcategories((apple, []), disableimage=True) == 'Apples'
+    assert has_more_subcategories((apple, []), headless=True, disableimage=True) == 'Apples'
+    assert len(has_more_subcategories((food, []), headless=True, disableimage=True)) == 15
+
+
+def test_load_more():
+    fruit = 'https://www.loblaws.ca/Food/Fruits-%26-Vegetables/Fruit/c/LSL001001001000?navid=CLP-L4-Fruit'
+    apple = 'https://www.loblaws.ca/Food/Fruits-%26-Vegetables/Fruit/Apples/plp/LSL001001001001?navid=CLP-L5-Apples'
+
+    web_driver_loc = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'chromedriver.exe')
+
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+    options.add_argument("window-size=1920,1080")
+    options.add_argument('--blink-settings=imagesEnabled=false')
+    driver = webdriver.Chrome(web_driver_loc, options=options)
+    driver.get(fruit)
+    assert load_more(driver) == 3
+    driver.get(apple)
+    assert load_more(driver) == 0
